410e0f7cfc5800d046acbce0528b459dbf9be75c
phrank-leeng <ling.frank000@gmail.com>
2023-05-12 12:50:23 +0200

    example from tsan wiki

---
+++ src/example.h
@@ -0,0 +1,14 @@
+//
+// Created by unknown on 11.05.23.
+//
+
+#ifndef TSAN_V2_SEMINAR_EXAMPLE_H
+#define TSAN_V2_SEMINAR_EXAMPLE_H
+
+
+class example {
+
+};
+
+
+#endif //TSAN_V2_SEMINAR_EXAMPLE_H
