70f55efade46c55c359b744233c93de0150709ea
phrank-leeng <ling.frank000@gmail.com>
2023-06-06 16:00:35 +0200

    WIP

--- latex/listings/unsoundness.cpp
+++ latex/listings/unsoundness.cpp
@@ -20,7 +20,7 @@ void *Thread2(void *x) {
     return NULL;
 }
 
-void unsoundness::run() {
+int main() {
     pthread_t t[2];
     pthread_create(&t[0], NULL, Thread1, NULL);
     pthread_create(&t[1], NULL, Thread2, NULL);
